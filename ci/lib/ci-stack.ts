76ee78bd3d375b1d445ed64774100a8a25c80573
Halit <oskan96@gmail.com>
2024-01-21 11:59:37 +0000

    cd path

--- ci/lib/ci-stack.ts
+++ ci/lib/ci-stack.ts
@@ -7,13 +7,15 @@ export class CiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const baseDirectory = 'ci';
+
     new CodePipeline(this, 'cypress-ts-pipeline', {
       pipelineName: "cypress-ts-pipeline",
       selfMutation: true,
       synth: new ShellStep('Syhtn', {
         input: CodePipelineSource.gitHub('halitos/cypress-ts-ci-demo', 'main'),
-        commands: [ 'npm ci', 'npx cdk synth --quiet'],
-        primaryOutputDirectory: '/cdk.out',
+        commands: [`cd ${baseDirectory}`, 'npm ci', 'npx cdk synth --quiet'],
+        primaryOutputDirectory: `${baseDirectory}/cdk.out`,
       })
     })
 
