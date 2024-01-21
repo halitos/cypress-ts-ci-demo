0d37a195b1dd78e62a5fca209fbe3e8b96e90595
Halit <oskan96@gmail.com>
2024-01-21 15:20:35 +0000

    machines

--- ci/lib/ci-stack.ts
+++ ci/lib/ci-stack.ts
@@ -15,20 +15,36 @@ export class CiStack extends Stack {
       synth: new ShellStep('Syhtn', {
         input: CodePipelineSource.gitHub('halitos/cypress-ts-ci-demo', 'main'),
         commands: [`cd ${baseDirectory}`, 'npm ci', 'npx cdk synth --quiet'],
-        primaryOutputDirectory: `${baseDirectory}/cdk.out`,
+        primaryOutputDirectory: `${baseDirectory}/cdk.out`
       })
     })
 
 
     const e2eStage = pipeline.addStage(new PipelineStage(this, 'E2eStage', {
-      stageName: 'E2eTest',
+      stageName: 'E2eTest'
     }))
 
-    e2eStage.addPre(new CodeBuildStep('e2e-test' , {
+    e2eStage.addPre(new CodeBuildStep('e2e-test', {
       commands: [
+        // Install Linux dependencies for Cypress
+        'apt-get update && apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb',
+
+        // Cache the ~/.cache folder after running npm ci
+        'npm ci && mkdir -p ~/.cache && mv ~/.npm/_cacache ~/.cache/Cypress/npm',
+
+        // Cache the ~/.npm directory
+        'mkdir -p ~/.npm && mv ~/.npm/_cacache ~/.npm/Cypress',
+
         'npm ci',
+
         'npm run cypress:run'
       ]
     }))
+
+    // e2eStage.addPre(new ShellStep('RunCypressTests', {
+    //   commands: [
+    //     ...
+    //   ],
+    // }));
   }
 }
