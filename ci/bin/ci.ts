d507bbc361c2d8b11ba3ccd767fd94fb5205cccf
Halit <oskan96@gmail.com>
2024-01-21 11:45:03 +0000

    pipeline init

--- ci/bin/ci.ts
+++ ci/bin/ci.ts
@@ -19,3 +19,5 @@ new CiStack(app, 'CiStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+app.synth()
--- ci/lib/ci-stack.ts
+++ ci/lib/ci-stack.ts
@@ -7,15 +7,12 @@ export class CiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-
-    const baseDirectory = 'ci';
-
     new CodePipeline(this, 'cypress-ts-pipeline', {
       pipelineName: "cypress-ts-pipeline",
       synth: new ShellStep('Syhtn', {
         input: CodePipelineSource.gitHub('halitos/cypress-ts-ci-demo', 'main'),
-        commands: [`cd ${baseDirectory}`, 'npm ci', 'npm run cdk synth --quiet'],
-        primaryOutputDirectory: `${baseDirectory}/cdk.out`,
+        commands: [ 'npm ci', 'npm run cdk synth --quiet'],
+        primaryOutputDirectory: '/cdk.out',
       })
     })
 
